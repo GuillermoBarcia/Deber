8f6ca97eb6baea2ff55236b68bc2484347678c33
Guillermo <Guillermo@DESKTOP-KUVSCE8>
2019-05-02 16:09:36 -0500

    segunda

--- src/deber/Morales.java
+++ src/deber/Morales.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package deber;
 
 /**
@@ -10,5 +6,5 @@ package deber;
  * @author Bryan jurado
  */
 public class Morales {
-    
+    //Mi primero yo
 }
